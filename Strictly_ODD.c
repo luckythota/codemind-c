f84d1c9978a6225844996892d028febddb552e8b
luckythota <97425859+luckythota@users.noreply.github.com>
2022-02-25 21:51:27 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,count=0;     for(i=0; i<n; i++)     {         scanf("%d",&a[i]);     }     for(i=0; i<n; i++)     {         if(i%2==0 && a[i]%2!=0)         {             count++;         }     }     if(count==0)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,count=0;
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0; i<n; i++)
+    {
+        if(i%2==0 && a[i]%2!=0)
+        {
+            count++;
+        }
+    }
+    if(count==0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
