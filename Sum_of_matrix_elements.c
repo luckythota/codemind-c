53537f4c08c32c3de5e84e88cde96cd029808757
luckythota <97425859+luckythota@users.noreply.github.com>
2022-02-25 20:19:38 +0530

    #include<stdio.h> int main() {     int rs,cs;     scanf("%d%d",&rs,&cs);     int a[rs][cs],i,j,sum=0;     for(i=0; i<rs; i++)     {         for(j=0; j<cs; j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0; i<rs; i++)     {         for(j=0; j<cs; j++)         {             sum=sum+a[i][j];         }     }     printf("%d",sum); }

---
+++ Sum_of_matrix_elements.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int rs,cs;
+    scanf("%d%d",&rs,&cs);
+    int a[rs][cs],i,j,sum=0;
+    for(i=0; i<rs; i++)
+    {
+        for(j=0; j<cs; j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0; i<rs; i++)
+    {
+        for(j=0; j<cs; j++)
+        {
+            sum=sum+a[i][j];
+        }
+    }
+    printf("%d",sum);
+}
