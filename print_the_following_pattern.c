eac59002839e6c6107af8727e6d058aabc6ed0ca
luckythota <97425859+luckythota@users.noreply.github.com>
2022-10-12 11:06:36 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=0; i<n; i++)     {         for(j=0; j<n; j++)         {             printf("%c ",(i+65));         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,j;
-    int i;
+    int n,i,j;
     scanf("%d",&n);
-    for(i=n-1; i>=0; i--)
+    for(i=0; i<n; i++)
     {
-        for(j=0; j<=i; j++)
+        for(j=0; j<n; j++)
         {
-            printf("%c ",i+65);
+            printf("%c ",(i+65));
         }
         printf("
 ");
